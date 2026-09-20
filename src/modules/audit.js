7b69da99aab65aaf0c871b0bd4d2eb5c4524f834
Mohamed Tharwat <Mohamedtharwat8>
2026-09-20 18:02:59 +0300

    feat: remove audit logging module

--- src/modules/audit.js
+++
@@ -1,7 +0,0 @@
-const prisma = require('../db/prisma');
-
-// Pass a transaction client as `db` to log inside the same transaction.
-const audit = (actorId, action, entity, entityId, db = prisma) =>
-  db.auditLog.create({ data: { actorId, action, entity, entityId } });
-
-module.exports = audit;
